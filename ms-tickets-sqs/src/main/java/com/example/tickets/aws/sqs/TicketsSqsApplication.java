c9916ab5a6000db61765f10a938683c9cb7bd8fb
Carlos Gamboa H <155551458+cgamboa-bayview@users.noreply.github.com>
2024-12-04 13:40:45 -0500

    sqs support sending messages

---
+++ ms-tickets-sqs/src/main/java/com/example/tickets/aws/sqs/TicketsSqsApplication.java
@@ -0,0 +1,13 @@
+package com.example.tickets.aws.sqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsSqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketsSqsApplication.class, args);
+	}
+
+}
