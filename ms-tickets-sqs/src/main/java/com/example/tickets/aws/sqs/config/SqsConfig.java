9ed717ac9ce5620e16d75826aff64b05b14416eb
Carlos Gamboa H <155551458+cgamboa-bayview@users.noreply.github.com>
2024-12-04 16:10:07 -0500

    sqs support sending messages sdk v2

--- ms-tickets-sqs/src/main/java/com/example/tickets/aws/sqs/config/SqsConfig.java
+++ ms-tickets-sqs/src/main/java/com/example/tickets/aws/sqs/config/SqsConfig.java
@@ -1,13 +1,9 @@
 package com.example.tickets.aws.sqs.config;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.sqs.AmazonSQS;
-import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.services.sqs.SqsClient;
 
 @Configuration
 public class SqsConfig {
@@ -19,13 +15,8 @@ public class SqsConfig {
     private String secretKey;
 
     @Bean
-    public AmazonSQS amazonSQSClient() {
-        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
-
-        return AmazonSQSClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
-                .withRegion(Regions.US_EAST_2)
-                .build();
+    public SqsClient sqsClient() {
+        return SqsClient.create();
     }
 
 }
--- ms-tickets-sqs/src/main/java/com/example/tickets/aws/sqs/service/IQueueService.java
+++ ms-tickets-sqs/src/main/java/com/example/tickets/aws/sqs/service/IQueueService.java
@@ -1,14 +1,14 @@
 package com.example.tickets.aws.sqs.service;
 
 
-import com.amazonaws.services.sqs.AmazonSQS;
-import com.amazonaws.services.sqs.model.GetQueueUrlResult;
 import com.example.tickets.aws.sqs.TicketMessage;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -21,19 +21,22 @@ public interface IQueueService {
 @RequiredArgsConstructor
 @Slf4j
 class AwsSqsService implements IQueueService {
-    private final AmazonSQS amazonSQSClient;
+    private final SqsClient sqsClient;
     private final ObjectMapper objectMapper;
-    @Value("${aws.queuename}")
-    private String queueName;
+    @Value("${aws.queue-url}")
+    private String queueUrl;
 
     @Override
     public TicketMessage publishMessage(TicketMessage ticketMessageRequest) {
         try {
-            GetQueueUrlResult queueUrl = amazonSQSClient.getQueueUrl(queueName);
             ticketMessageRequest.setId(UUID.randomUUID().toString());
             ticketMessageRequest.setCreatedAt(LocalDateTime.now());
             log.info("Request {}", ticketMessageRequest);
-            amazonSQSClient.sendMessage(queueUrl.getQueueUrl(), objectMapper.writeValueAsString(ticketMessageRequest));
+            sqsClient.sendMessage(SendMessageRequest.builder()
+                    .delaySeconds(1)
+                    .queueUrl(queueUrl)
+                    .messageBody(objectMapper.writeValueAsString(ticketMessageRequest))
+                    .build());
             return ticketMessageRequest;
         } catch (Exception e) {
             log.error("Queue Exception Message: {}", e.getMessage());
